c3007dfe67b16aaf4f422fbfbf1a7e12013b0b0e
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-08-22 22:58:23 -0500

    Version 0.0.1

---
+++ data-updates.lua
@@ -0,0 +1,27 @@
+if mods["IndustrialRevolution3"]
+    and mods["IndustrialRevolution3LoadersStacking"]
+    and mods["IntermodalContainers"]
+then
+    data.raw.recipe["ic-containerization-machine-1"].ingredients = {
+        {"iron-frame-large", 1},
+        {"computer-mk1", 1},
+        {"ir3-loader", 2},
+        {"ir3-beltbox", 1}
+    }
+    data.raw.recipe["ic-containerization-machine-2"].ingredients = {
+        {"steel-frame-large", 1},
+        {"computer-mk2", 1},
+        {"ir3-fast-loader", 2},
+        {"ir3-fast-beltbox", 1}
+    }
+    data.raw.recipe["ic-containerization-machine-3"].ingredients = {
+        {"chromium-frame-large", 1},
+        {"computer-mk3", 1},
+        {"ir3-express-loader", 2},
+        {"ir3-express-beltbox", 1}
+    }
+
+    data.raw.technology["ic-containerization-1"].prerequisites = {"ir3-beltbox", "ir-electronics-1"}
+    data.raw.technology["ic-containerization-2"].prerequisites = {"ir3-fast-beltbox", "ir-electronics-2"}
+    data.raw.technology["ic-containerization-3"].prerequisites = {"ir3-express-beltbox", "ir-electronics-3"}
+end
---
+++ readme.md
@@ -0,0 +1,4 @@
+Helps to integrate Industrial Revolution 3 and IR3:Powered Loaders & Ingot Stacking with Intermodal Containers.
+Adjusting containerization machine recipes and technology prerequisites to be more aligned with Industrial Revolution 3.
+Recipes require a level appropriate computer, large frame, stacker and two loaders.
+Technology prerequisites are the level approriate stacker and electronics researches.
